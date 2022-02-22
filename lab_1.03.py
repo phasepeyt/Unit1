653cd87add64e30cb6cda2b2269b96877ddc9d3f
peyton <peytonharris87@gmail.com>
2022-02-22 09:21:23 -0600

    added file

---
+++ lab_1.03.py
@@ -0,0 +1,34 @@
+'''
+Lab 1.03 - Printing & Variables
+
+Part 1 - Printing Practice
+Practice typing out some statements in the editor part of the IDE, then hit "Run" at the top of the screen:
+**Remember you must exit() python interpreter before running a script!!!
+
+    Expression                                      Expected Output                               Did anything unexpected happen?
+a)  print("1")                                      "1"                                                 no
+b)  print(1)                                        1                                                   
+c)  print(1 + 2)                                    3                                                   
+d)  print("1" + "2")                                  12                                                 
+e)  print("this" + " " + "is" + " " + 
+    "a" + " " +
+    "sentence" + ".")                              this is a sentence                                   invalid syntax        
+
+Part 2 - Variables Practice
+1. In your Console
+Type and run the following:
+    animal = "dogs"
+    print(animal + " are really cool.")             dogs are really cool
+In your Notebook
+Respond to the following:
+    1. What happens?    it says dogs are really cool
+    2. How would you make the program print out "cats are really cool" instead?     do animal = "cats"
+
+2. In your Console
+Type and run the following code:
+print(dogs + " are cool.")
+In your Notebook
+Respond to the following questions:
+    1. What output does this produce?  invalid syntax
+    2. Why does this happen?  dogs is not in quotes
+'''
---
+++ mad_libs.py
@@ -0,0 +1,11 @@
+## madlibs
+city = input("name a city ")
+adj1 = input("name an adjective ")
+noun = input("name a noun ")
+pronoun = input("name a pronoun ")
+adj2 = input("name another adjective ")
+activity = input("name an activity ")
+verb = input("name a verb ")
+num = input("name a number ")
+
+print(f"one day in {city}, a {adj1} {noun} was walking down the street. {pronoun} was very {adj2}, and really liked {activity}. {noun} then {verb} {num} times!! a world record. finally, {noun} left, and it was normal again")
